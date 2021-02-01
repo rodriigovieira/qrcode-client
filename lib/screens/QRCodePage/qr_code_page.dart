5afd8ca9e291ea84ac004d9f05a511400d5880eb
Rodrigo Vieira <rodriigovieira@gmail.com>
2021-02-01 02:00:14 -0300

    creating initial version of scan/qr code pages

--- lib/screens/QRCodePage/qr_code_page.dart
+++ lib/screens/QRCodePage/qr_code_page.dart
@@ -1,21 +1,95 @@
-import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+import 'dart:async';
+import 'dart:convert';
 
-class QRCodePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qrcode_client/constants.dart';
+import 'package:qrcode_client/models/seed_model.dart';
+
+class QRCodePage extends StatefulWidget {
   static const String pageId = "/qr_code_page";
 
   @override
+  _QRCodePageState createState() => _QRCodePageState();
+}
+
+class _QRCodePageState extends State<QRCodePage> {
+  bool loading = false;
+  String seed = "";
+  int secondsLeft = 0;
+
+  void updateTimeLeft() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (secondsLeft < 1) {
+        timer.cancel();
+        return;
+      }
+
+      setState(() {
+        secondsLeft--;
+      });
+    });
+  }
+
+  void handleLoading() async {
+    var response = await get("$kAPIBaseUrl/seed");
+
+    if (response.statusCode == 200) {
+      SeedModel data = SeedModel.fromJson(jsonDecode(response.body));
+
+      DateTime expireDate = DateTime.parse(data.expiresAt);
+      Duration timeLeft = expireDate.difference(DateTime.now());
+
+      setState(() {
+        loading = false;
+        seed = data.seed;
+        secondsLeft = timeLeft.inSeconds;
+      });
+
+      updateTimeLeft();
+    }
+  }
+
+  @override
+  void initState() {
+    handleLoading();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (loading) {
+      return Scaffold(
+        appBar: AppBar(title: Text("QR Code")),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("QR Code"),
       ),
       body: Center(
-        child: QrImage(
-          version: QrVersions.auto,
-          size: 320,
-          gapless: false,
-          data: "test",
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            QrImage(
+              version: QrVersions.auto,
+              size: 320,
+              gapless: false,
+              data: seed,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "$secondsLeft\s",
+                style: TextStyle(fontSize: 16),
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/screens/ScanPage/scan_page.dart
+++ lib/screens/ScanPage/scan_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qrcode_client/constants.dart';
 
 class ScanPage extends StatefulWidget {
   static const String pageId = "/scan_page";
@@ -11,15 +15,51 @@ class ScanPage extends StatefulWidget {
 class _ScanPageState extends State<ScanPage> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
 
-  void onQRViewCreated(QRViewController controller) async {
-    controller.scannedDataStream.listen((Barcode event) {
-      print(event.code);
+  StreamSubscription cameraStreamSubscription;
+  Stream cameraStream;
+  
+  bool isValid = false;
+  bool hasLoadedCode = false;
+
+  void scanAgain() {
+    setState(() {
+      hasLoadedCode = false;
     });
+
+    cameraStreamSubscription = cameraStream.listen((event) {
+      checkIfValid(event.code);
+      cameraStreamSubscription.cancel();
+    });
+  }
+
+  void onQRViewCreated(QRViewController controller) async {
+    cameraStream = controller.scannedDataStream.asBroadcastStream();
+
+    cameraStreamSubscription = cameraStream.listen((event) {
+      checkIfValid(event.code);
+      cameraStreamSubscription.cancel();
+    });
+  }
+
+  void checkIfValid(String data) async {
+    var response = await get("$kAPIBaseUrl/seed/$data");
+
+    if (response.statusCode == 200) {
+      setState(() {
+        isValid = response.body == "true";
+        hasLoadedCode = true;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cameraStreamSubscription.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
-    print("ae");
     return Scaffold(
       appBar: AppBar(
         title: Text("Scan"),
@@ -30,13 +70,58 @@ class _ScanPageState extends State<ScanPage> {
             "Scan the QR Code to check whether it's still valid or not.",
           ),
           Expanded(
+            flex: 4,
             child: Center(
               child: Container(
                 height: 300,
                 width: 300,
-                child: QRView(
-                  onQRViewCreated: onQRViewCreated,
-                  key: qrKey,
+                child: Stack(
+                  children: [
+                    Opacity(
+                      opacity: hasLoadedCode ? 0.3 : 1,
+                      child: QRView(
+                        onQRViewCreated: onQRViewCreated,
+                        key: qrKey,
+                      ),
+                    ),
+                    Visibility(
+                      visible: hasLoadedCode,
+                      child: InkWell(
+                        onTap: scanAgain,
+                        child: Center(
+                          child: Container(
+                            height: 120,
+                            width: 120,
+                            padding: EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(Icons.refresh, size: 28),
+                                SizedBox(height: 5),
+                                Text("Scan again?"),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Visibility(
+              visible: hasLoadedCode,
+              child: Text(
+                isValid ? "The QR Code is valid!" : "The QR Code is not valid!",
+                style: TextStyle(
+                  color: isValid ? Colors.green : Colors.red,
                 ),
               ),
             ),

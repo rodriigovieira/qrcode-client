df892e4ee4b7b03f3a655267d03a5b9b8e87599a
Rodrigo Vieira <rodriigovieira@gmail.com>
2021-02-02 20:32:41 -0300

    changing api base url to live preview

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 /// This file contains all constants
 /// of the project, so typos and repetition
 /// can be avoided.
-const String kAPIBaseUrl = "http://localhost:3000";
+const String kAPIBaseUrl = "https://qrcode-generator-backend-app.herokuapp.com";

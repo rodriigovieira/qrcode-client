16d381b4d9f3a28d535935c4a52c09a4aa87ddac
Rodrigo Vieira <rodriigovieira@gmail.com>
2021-02-02 15:51:47 -0300

    changing api base url back to localhost

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 /// This file contains all constants
 /// of the project, so typos and repetition
 /// can be avoided.
-const String kAPIBaseUrl = "http://192.168.1.168:3000";
+const String kAPIBaseUrl = "http://localhost:3000";

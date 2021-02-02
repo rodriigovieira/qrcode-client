b7c98ec2eacc7419cd3732ee527855554d473302
Rodrigo Vieira <rodriigovieira@gmail.com>
2021-02-01 22:24:15 -0300

    adding caching solution

--- lib/services/http_client.dart
+++ lib/services/http_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:qrcode_client/constants.dart';
 import 'package:qrcode_client/interfaces/http_client_interface.dart';
 
@@ -12,11 +13,16 @@ class ClientHttpService implements IClientHttp {
         receiveTimeout: 10000,
       ),
     );
+
+    dio.interceptors.add(DioCacheManager(CacheConfig()).interceptor);
   }
 
   @override
   Future get(String path) async {
-    var response = await dio.get(path);
+    var response = await dio.get(
+      path,
+      options: buildCacheOptions(Duration(minutes: 1)),
+    );
 
     return response.data;
   }
